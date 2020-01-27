42b41d4f38af095387e6193c7ce8f668ace2e49b
Raphael Huefner <raphael@upvest.co>
2020-01-27 14:32:39 +0100

    Add code snippet for verifying webhooks HMACs

---
+++ js/webhooks/verifyHmac.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto');
+
+function verifyUpvestWebhookHmac(requestHeaders, rawRequestBody, hmacSecretKey) {
+  const signatureHeader = requestHeaders['X-Up-Signature'];
+  if (!signatureHeader) {
+    return false;
+  }
+  const hmac = crypto.createHmac('sha256', hmacSecretKey).update(rawRequestBody, 'utf8').digest('hex');
+  return signatureHeader == `sha256=${hmac}`;
+}
+
+module.exports = { verifyUpvestWebhookHmac };
