e5798b3c90ae93f7dab0126df6e3bfd9630ef7c5
Raphael Huefner <raphael@upvest.co>
2020-02-13 09:14:52 +0100

    Improve webhook HMAC verifier example

--- js/webhooks/verifyHmac.js
+++ js/webhooks/verifyHmac.js
@@ -1,10 +1,26 @@
 const crypto = require('crypto');
 
-function verifyUpvestWebhookHmac(requestHeaders, rawRequestBody, hmacSecretKey) {
-  const signatureHeader = requestHeaders['X-UP-Signature'];
-  if (!signatureHeader) {
-    return false;
+function isUpvestHmacHeaderName(headerName) {
+  headerName = headerName
+    .toLowerCase()
+    .replace(/[^a-z]+/g, '-') // Neutralize all non-alphabetical characters
+    .replace(/^http-/g, '') // Some environments *might* have an "HTTP_" prefix
+    .replace(/(^-*|-*$)/g, '') // Remove leading and trailing dashes
+  ;
+  return headerName == 'x-up-signature';
+}
+
+function extractUpvestHmacHeader(requestHeaders) {
+  for (const [name, value] of Object.entries(requestHeaders)) {
+    if (isUpvestHmacHeaderName(name)) {
+      return value;
+    }
   }
+  throw new Error('No Upvest HMAC header "X-UP-Signature".');
+}
+
+function verifyUpvestWebhookHmac(requestHeaders, rawRequestBody, hmacSecretKey) {
+  const signatureHeader = extractUpvestHmacHeader(requestHeaders);
   const hmac = crypto.createHmac('sha256', hmacSecretKey).update(rawRequestBody, 'utf8').digest('hex');
   return signatureHeader == `sha256=${hmac}`;
 }
